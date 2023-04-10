524ccf12068b5f1cddef029a7f08bca39511ccb1
NiniPapuna <106619928+NiniPapuna@users.noreply.github.com>
2023-04-10 00:23:12 -0600

    changes to wa11

--- js/wa11.js
+++ js/wa11.js
@@ -1,10 +1,13 @@
-
 window.addEventListener('load', getQuote);
 
 const quoteButton = document.querySelector(".new-quote");
 quoteButton.addEventListener('click', getQuote);
 
-const endpoint = 'https://dog.ceo/api/breeds/image/random';
+const imgButton = document.querySelector(".new-img");
+imgButton.addEventListener('click', getImage);
+
+const endpoint = 'https://www.boredapi.com/api/activity';
+const endpoint1 = 'https://dog.ceo/api/breeds/image/random';
 
 async function getQuote(){
     let text = await fetch(endpoint);
@@ -12,13 +15,29 @@ async function getQuote(){
 
     let jsonResponse = JSON.parse(response);
     console.log(jsonResponse);
-    console.log(jsonResponse['message']);
+    console.log(jsonResponse['activity']);
 
-    displayImage(jsonResponse['message']);
+    displayQuote(jsonResponse['activity']);
+}
+
+async function getImage(){
+    let t = await fetch(endpoint1);
+    let res = await t.text();
+
+    let jsResponse = JSON.parse(res);
+    console.log(jsResponse);
+    console.log(jsResponse['message']);
+
+    displayImage(jsResponse['message']);
+}
+
+function displayQuote(x){
+    document.getElementById('js-quote-text').textContent = x;
 }
 
 function displayImage(imageUrl) {
-  const img = document.createElement('img');
-  img.src = imageUrl;
-  document.body.appendChild(img); 
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    document.getElementById('js-img').innerHTML = ''; // clear previous image
+    document.getElementById('js-img').appendChild(img); // add the new image
 }
